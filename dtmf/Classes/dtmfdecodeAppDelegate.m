92fa476e71dd411fea4f5fd8cc470f886970c7fd
veg <veg@3ca81f0b-09e3-5f19-db2a-264bbb327774>
2009-09-26 17:35:09 +0000

    Add GPL gunge

--- dtmf/Classes/dtmfdecodeAppDelegate.m
+++ dtmf/Classes/dtmfdecodeAppDelegate.m
@@ -1,10 +1,24 @@
-//
-//  dtmfdecodeAppDelegate.m
-//  dtmfdecode
-//
-//  Created by Veghead on 14/11/2008.
-//  Copyright __MyCompanyName__ 2008. All rights reserved.
-//
+/*
+ 
+ $Id$
+ 
+ Dreadtech DTMF Decoder - Copyright 2009 Martin Wellard
+ 
+ This file is part of Dreadtech DTMF Decoder.
+ 
+ Dreadtech DTMF Decoder is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Dreadtech DTMF Decoder is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Dreadtech DTMF Decoder.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import "dtmfdecodeAppDelegate.h"
 #import "dtmfdecodeViewController.h"
